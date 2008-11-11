8bfc3996d34df407734b417f3d5c027cd7957eb3
Christian Hammond <christian@vmware.com>
2008-11-10 16:51:33 -0800

    Fix a syntax error.

--- support-scripts/get-fuzzy-file-finder.sh
+++ support-scripts/get-fuzzy-file-finder.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if test ! -e /usr/bin/gem; then
-	if -e /usr/bin/apt-get; then
+	if test -e /usr/bin/apt-get; then
 		sudo apt-get install rubygems
 	else
 		echo "The ruby gems package is needed."
